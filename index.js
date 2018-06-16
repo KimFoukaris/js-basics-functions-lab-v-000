ed9bab67311e62a98f87ec9bb73627f56a361076
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-16 13:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ function distanceTravelledInFeet(beginningBlock, endingBlock) {
   return distance
 }
 
+function calculatesFarePrice(start, destination) {
+  if (distanceTravelledInFeet < 400) {
+    fare = 0;
+    return fare
+  } else if (distanceTravelledInFeet > 2500) {
+    message = "cannot travel that far";
+    return message
+  } else if (distanceTravelledInFeet > 2000) {
+    rate = 25;
+    return rate
+  } else {
+    rate = Math.abs(distanceTravelledInFeet-400)*264;
+    return rate
+  }
+}
+    
+
+  (distanceTravelledInFeet(start, destination)-400)*.02
+}
