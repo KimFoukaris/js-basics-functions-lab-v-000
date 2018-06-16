616d14ad2c9c559ac05ea37df5919c9d1cbbc4b9
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-16 13:21:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function distanceFromHqInBlocks(beginningBlock) {
-  distance = Math.abs(beginningBlock-42)
+  distance = Math.abs(beginningBlock-42);
+  return distance
+}
+
+function distanceFromHqInFeet(beginningBlock) {
+  distanceFromHqInBlocks(beginningBlock)*264;
   return distance
 }
